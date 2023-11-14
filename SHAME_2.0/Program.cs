de13b5a159540d0af088f63d9fe69e35f50ef1bf
M1sp3ro <102689396+M1sp3ro@users.noreply.github.com>
2023-11-14 22:49:59 +0300

    3*
    3*

--- SHAME_2.0/Program.cs
+++ SHAME_2.0/Program.cs
@@ -170,18 +170,7 @@ namespace SHAME_2._0
                 _data = new Data[num_of_added + 1];
                 for (int i = 0; i < _data.Length; i++)
                     _data[i] = new Data();
-                string input = Console.ReadLine();
-                Console.Write("Введите фамилию: ");
-                if (String.IsNullOrEmpty(input))
-                {
-                    init.surname = Console.ReadLine();
-                }
-                else
-                {
-                    Console.WriteLine("Пустая строка запрещена.");
-                    return;
-                }
-
+                
                 // вводим нужные данные 
                 Console.Write("Введите фамилию: ");
                 init.surname = Console.ReadLine();
