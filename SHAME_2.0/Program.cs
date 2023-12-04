a6e9c3202a02dd7008edee059e9022140d00cc0f
M1sp3ro <102689396+M1sp3ro@users.noreply.github.com>
2023-12-04 12:09:48 +0300

    last
    last

--- SHAME_2.0/Program.cs
+++ SHAME_2.0/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Text.Json;
+using System.Text.Unicode;
 using System.Data;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Text.Encodings.Web;
 
 namespace StudentDatabase
 {
@@ -15,7 +17,7 @@ namespace StudentDatabase
             {
                 Console.WriteLine("Выберите действие: ");
                 Console.WriteLine("======================================");
-                Console.WriteLine("1. Добавить студента (ну или студентку");
+                Console.WriteLine("1. Добавить студента");
                 Console.WriteLine("2. Посмотреть информацию");
                 Console.WriteLine("======================================");
                 Console.WriteLine("3. Сохранить файл");
@@ -190,180 +192,8 @@ namespace StudentDatabase
             curriculum.group = InputNoSpaces("Номер группы: ");
             data[numOfAdded].DataEntry(init, curriculum);
 
-/*
-
-            if (numOfAdded == 0)
-            {
-
-                data = new Data[numOfAdded + 1];
-                for (int i = 0; i < data.Length; i++)
-                    data[i] = new Data();
-
-
-                do
-                {
-                    Console.Write("Фамилия: ");
-                    init.surname = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    init.surname = regex.Replace(init.surname, string.Empty);
-                    Console.Clear();
-
-                } while (init.surname == "");
-
-
-                do
-                {
-                    Console.Write("Имя: ");
-                    init.name = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    init.name = regex.Replace(init.name, string.Empty);
-                    Console.Clear();
-
-                } while (init.name == "");
-
-                do
-                {
-                    Console.Write("Отчетсво: ");
-                    init.patronymic = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    init.patronymic = regex.Replace(init.patronymic, string.Empty);
-                    Console.Clear();
-
-                } while (init.patronymic == "");
-
-                do
-                {
-                    Console.Write("Название факультета: ");
-                    curriculum.faculty = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.faculty = regex.Replace(curriculum.faculty, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.faculty == "");
-
-                do
-                {
-                    Console.Write("Название специальности: ");
-                    curriculum.speciality = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.speciality = regex.Replace(curriculum.speciality, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.speciality == "");
-
-                do
-                {
-                    Console.Write("Номер курса: ");
-                    curriculum.course = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.course = regex.Replace(curriculum.course, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.course == "");
-
-                do
-                {
-                    Console.Write("Номер группы: ");
-                    curriculum.group = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.group = regex.Replace(curriculum.group, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.group == "");
-
-                data[0].DataEntry(init, curriculum);
-            }
-            else
-            {
-                // копируем массив данных с помощью метода Close()
-                // иначе, можно создать новый массив к каждому элементу нового присвоить значение старого массива в цикле 
-                Data[] buf_data = (Data[])data.Clone();
-
-                data = new Data[numOfAdded + 1];
-                for (int i = 0; i < data.Length; i++)
-                    data[i] = new Data();
-
-                for (int i = 0; i < buf_data.Length; i++)
-                    data[i] = buf_data[i];
-
-                do
-                {
-                    Console.Write("Фамилия: ");
-                    init.surname = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    init.surname = regex.Replace(init.surname, string.Empty);
-                    Console.Clear();
-
-                } while (init.surname == "");
-
-                do
-                {
-                    Console.Write("Имя: ");
-                    init.name = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    init.name = regex.Replace(init.name, string.Empty);
-                    Console.Clear();
-
-                } while (init.name == "");
-
-                do
-                {
-                    Console.Write("Отчетсво: ");
-                    init.patronymic = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    init.patronymic = regex.Replace(init.patronymic, string.Empty);
-                    Console.Clear();
-
-                } while (init.patronymic == "");
-
-                do
-                {
-                    Console.Write("Название факультета: ");
-                    curriculum.faculty = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.faculty = regex.Replace(curriculum.faculty, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.faculty == "");
-
-                do
-                {
-                    Console.Write("Название специальности: ");
-                    curriculum.speciality = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.speciality = regex.Replace(curriculum.speciality, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.speciality == "");
-
-                do
-                {
-                    Console.Write("Номер курса: ");
-                    curriculum.course = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.course = regex.Replace(curriculum.course, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.course == "");
-
-                do
-                {
-                    Console.Write("Номер группы: ");
-                    curriculum.group = Console.ReadLine();
-                    Regex regex = new Regex(@"\s+");
-                    curriculum.group = regex.Replace(curriculum.group, string.Empty);
-                    Console.Clear();
-
-                } while (curriculum.group == "");
-
-                data[numOfAdded].DataEntry(init, curriculum);
-
-                Array.Clear(buf_data, 0, buf_data.Length);
-            }
-*/
             Console.Clear();
             Console.WriteLine("Данные добавлены!");
-
         }
 
         static void DataReading(ref Data[] data)
@@ -381,69 +211,13 @@ namespace StudentDatabase
                 data = JsonSerializer.Deserialize<Data[]>(json);    //десериализация прочитанных данных
                 reading.Close();
 
-                /*
-                int numOfData = Convert.ToInt32(reading.ReadLine());
-                int i = 0,
-                numOfLine = 0;
-
-                data = new Data[numOfData];
-                for (; i < data.Length; i++)
-                    data[i] = new Data();
-                i = 0;
-
-                Initials init;
-                Сurriculum curriculum;
-
-                init.name = "";
-                init.surname = "";
-                init.patronymic = "";
-
-                while (!reading.EndOfStream)
-                {
-                    if (numOfLine == 0)
-                    {
-                        init.surname = reading.ReadLine();
-                        numOfLine++;
-                    }
-                    else if (numOfLine == 1)
-                    {
-                        init.name = reading.ReadLine();
-                        numOfLine++;
-                    }
-                    else if (numOfLine == 2)
-                    {
-                        init.patronymic = reading.ReadLine();
-                        numOfLine++;
-                    }
-
-                    else if (numOfLine == 3)
-                    {
-
-                        string line = reading.ReadLine();
-                        string[] splitline = line.Split(' ');
-
-                        curriculum.faculty = splitline[0];
-                        curriculum.speciality = splitline[1];
-                        curriculum.course = splitline[2];
-                        curriculum.group = splitline[3];
-
-                        data[i].DataEntry(init, curriculum);
-
-                        i++;
-                    }
-                }
-                */
                 Console.WriteLine($"Данных: {data.Length} считано из файла: {fileName}");
             }
             catch
             {
                 Console.WriteLine($"Ошибка при работе с файлом: {fileName} !");
             }
-/*            finally
-            {
-                Array.Resize(ref data, data.Length - 1);
-            }
-*/
+
         }
 
         static void Print(Data[] data)
@@ -467,24 +241,14 @@ namespace StudentDatabase
             Console.Clear();
             try
             {
+                var options = new JsonSerializerOptions
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    WriteIndented = true
+                };
                 StreamWriter record = new StreamWriter(fileName, false);        //лучше создние файла тоже в try взять
-                string jsonString = JsonSerializer.Serialize(data);
+                string jsonString = JsonSerializer.Serialize(data, options);
                 record.Write(jsonString);
-
-
-                /*                record.WriteLine(data.Length);
-
-                                foreach (Data d in data)
-                                {
-                                    record.WriteLine(d.GetInitials().surname);
-                                    record.WriteLine(d.GetInitials().name);
-                                    record.WriteLine(d.GetInitials().patronymic);
-                                    record.WriteLine(d.GetCurriculum().faculty);
-                                    record.WriteLine(d.GetCurriculum().speciality);
-                                    record.WriteLine(d.GetCurriculum().course);
-                                    record.WriteLine(d.GetCurriculum().group);
-                                }
-                */
                 record.Close();
 
                 Console.WriteLine($"Данных: {data.Length} записанно в файл: {fileName}");
--- SHAME_2.0/StudentData.cs
+++ SHAME_2.0/StudentData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace StudentDatabase
